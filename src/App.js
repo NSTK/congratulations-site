09d746ab09e4999a4af37fb3646f995272a5411c
Nikita <nikita.skripka@gmail.com>
2024-12-01 01:34:26 +0300

    Add confetti

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Confetti from "react-confetti";
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="app-container">
       <h1>С Днем Рождения!</h1>
+      {isOpen && <Confetti numberOfPieces={300} />}
       <div className={`envelope ${isOpen ? 'open' : 'closed'}`} onClick={toggleEnvelope}>
         <div className="flap top"></div>
         <div className="flap bottom">Открыть</div>
