7467c6f434f23a3d8930d19bd0b7d3b9526bee1f
Nikita <nikita.skripka@gmail.com>
2024-12-02 13:42:51 +0300

    Add photo

--- src/App.js
+++ src/App.js
@@ -12,14 +12,21 @@ function App() {
   return (
     <div className="app-container">
       <h1>С Днем Рождения!</h1>
-      {isOpen && <Confetti numberOfPieces={300} />}
+      {isOpen && <Confetti numberOfPieces={1500} />}
       <div className={`envelope ${isOpen ? 'open' : 'closed'}`} onClick={toggleEnvelope}>
         <div className="flap top"></div>
         <div className="flap bottom">Открыть</div>
         <div className="body">
-          <p className={`message ${isOpen ? 'show' : 'hide'}`}>
+          <p className={`message ${isOpen ? "show" : "hide"}`}>
             Пусть каждый день будет полон радости, тепла и вдохновения!
           </p>
+          {isOpen && (
+            <img
+              src={process.env.PUBLIC_URL + "/images/photo.jpg"}
+              alt="Поздравление"
+              className="congratulation-photo"
+            />
+          )}
         </div>
       </div>
     </div>
